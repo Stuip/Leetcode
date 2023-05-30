f25098373170de2432e079c643e1ce6b32b3eeff
Stuip <jmtangee@gmail.com>
2023-05-30 10:46:05 +0800

    Pass Leetcode219

--- SlidingWindow/Leetcode219/main.go
+++ SlidingWindow/Leetcode219/main.go
@@ -2,6 +2,27 @@ package main
 
 import "fmt"
 
+func containsNearbyDuplicate(nums []int, k int) bool {
+	if len(nums) <= 1 {
+		return false
+	}
+	if k <= 0 {
+		return false
+	}
+	m := make(map[int]bool, len(nums))
+	for i, num := range nums {
+		if _, ok := m[num]; ok {
+			return true
+		}
+		m[num] = true
+		if len(m) == k+1 {
+			delete(m, nums[i-k])
+		}
+	}
+	return false
+}
+
 func main() {
-	fmt.Println("Hello LeetCode!")
+	nums := []int{1, 2, 3, 1, 2, 3}
+	fmt.Println(containsNearbyDuplicate(nums, 2))
 }
