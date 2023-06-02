6b7a82616ea6b03b6849db9e71afd149acf0f099
Stuip <jmtangee@gmail.com>
2023-06-02 08:25:49 +0800

    Add another approach:SlidingWindow

--- SlidingWindow/Leetcode3/main.go
+++ SlidingWindow/Leetcode3/main.go
@@ -29,7 +29,23 @@ func max(a, b int) int {
 	return b
 }
 
+func lengthOfLongestSubstring2(s string) int {
+	n := len(s)
+	m := make(map[byte]int, n)
+	ans, left, right := 0, 0, 0
+	for right < n {
+		if idx, ok := m[s[right]]; ok && idx >= left {
+			// 这个需要注意idx >= left, 因为在确保重复的字符在区间之内，而不是在区间之外
+			left = idx + 1
+		}
+		m[s[right]] = right
+		right++
+		ans = max(ans, right-left)
+	}
+	return ans
+}
+
 func main() {
-	s := "abcabcbb"
-	fmt.Println(lengthOfLongestSubstring1(s))
+	s := "abcasadasfdafsaabcbb"
+	fmt.Println(lengthOfLongestSubstring2(s))
 }
