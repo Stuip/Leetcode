0c3f151f737ad6f0d009208d8afa94bc412ee3b1
Stuip <tangjmee@gmail.com>
2023-06-05 20:00:16 +0800

    Pass Leetcode7

--- Math/Leetcode7/main.go
+++ Math/Leetcode7/main.go
@@ -1,1 +1,30 @@
-package Leetcode7
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func reverse(x int) int {
+	flag := false
+	if x < 0 {
+		flag = true
+		x = -x
+	}
+	reverseNum := 0
+	for x != 0 {
+		if reverseNum < math.MinInt32/10 || reverseNum > math.MaxInt32/10 {
+			return 0
+		}
+		reverseNum = reverseNum*10 + x%10
+		x /= 10
+	}
+	if flag {
+		reverseNum = -reverseNum
+	}
+	return reverseNum
+}
+
+func main() {
+	fmt.Println(reverse(9646324351))
+}
