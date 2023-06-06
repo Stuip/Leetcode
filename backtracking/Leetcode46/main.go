e7c85e2c07ae2a72a8b4728b3e385afc80b50da5
Stuip <tangjmee@gmail.com>
2023-06-06 20:38:31 +0800

    Change the directory name

--- backtracking/Leetcode46/main.go
+++ backtracking/Leetcode46/main.go
@@ -1,1 +1,30 @@
-package Leetcode46
+package main
+
+import "fmt"
+
+func permute(nums []int) [][]int {
+	ans, path := [][]int{}, []int{}
+	n := len(nums)
+	var backtrack func(startIdx int)
+	backtrack = func(startIdx int) {
+		if len(path) == n {
+			copyPath := make([]int, len(path))
+			copy(copyPath, path)
+			ans = append(ans, copyPath)
+			return
+		}
+		for i := startIdx; i < n; i++ {
+			path = append(path, nums[i])
+			backtrack(startIdx + 1)
+			path = path[:len(path)-1]
+		}
+	}
+	backtrack(0)
+	return ans
+}
+
+func main() {
+	nums := []int{1, 2, 3}
+	fmt.Println(permute(nums))
+
+}
