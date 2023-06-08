dd9298dea4df623dcd6fe497b254aadecb722c43
Stuip <tangjmee@gmail.com>
2023-06-08 12:30:49 +0800

    Pass Leetcode78 by another solution

--- backtracking/Leetcode78/main.go
+++ backtracking/Leetcode78/main.go
@@ -21,7 +21,29 @@ func subsets(nums []int) [][]int {
 	return ans
 }
 
+func subsets1(nums []int) [][]int {
+	ans := [][]int{}
+	path := []int{}
+	var backtracking func(startIdx int)
+	backtracking = func(startIdx int) {
+		if startIdx == len(nums) {
+			copyPath := make([]int, len(path))
+			copy(copyPath, path)
+			ans = append(ans, copyPath)
+			return
+		}
+		// 不选择nums[i], 直接跳过该数
+		backtracking(startIdx + 1)
+		path = append(path, nums[startIdx])
+		backtracking(startIdx + 1)
+		path = path[:len(path)-1]
+	}
+	backtracking(0)
+	return ans
+}
+
 func main() {
-	nums := []int{0}
+	nums := []int{1, 2, 3}
 	fmt.Println(subsets(nums))
+	fmt.Println(subsets1(nums))
 }
