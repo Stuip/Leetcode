9a38ed16f91101afcec6fcee428377c3b60c04a1
Stuip <tangjmee@gmail.com>
2023-06-16 22:19:26 +0800

    Add another method

--- backtracking/Leetcode90/main.go
+++ backtracking/Leetcode90/main.go
@@ -28,7 +28,29 @@ func subsetsWithDup(nums []int) [][]int {
 	return ans
 }
 
+func subsetsWithDup1(nums []int) [][]int {
+	ans := [][]int{}
+	path := []int{}
+	sort.Ints(nums)
+	var backtrack func(choosePre bool, startIdx int)
+	backtrack = func(choosePre bool, startIdx int) {
+		if startIdx == len(nums) {
+			ans = append(ans, append([]int{}, path...))
+			return
+		}
+		backtrack(false, startIdx+1)
+		if !choosePre && startIdx > 0 && nums[startIdx-1] == nums[startIdx] {
+			return
+		}
+		path = append(path, nums[startIdx])
+		backtrack(true, startIdx+1)
+		path = path[:len(path)-1]
+	}
+	backtrack(false, 0)
+	return ans
+}
+
 func main() {
 	nums := []int{1, 2, 2}
-	fmt.Println(subsetsWithDup(nums))
+	fmt.Println(subsetsWithDup1(nums))
 }
