5316c761fec4c81d7bc37c76bce2bc357de269c1
Stuip <tangjmee@gmail.com>
2023-06-20 21:06:44 +0800

    Pass change the leetcode46

--- backtracking/Leetcode46/main.go
+++ backtracking/Leetcode46/main.go
@@ -3,28 +3,55 @@ package main
 import "fmt"
 
 func permute(nums []int) [][]int {
-	ans, path := [][]int{}, []int{}
+	ans := [][]int{}
 	n := len(nums)
+	path := make([]int, n)
+	used := make([]bool, n)
 	var backtrack func(startIdx int)
 	backtrack = func(startIdx int) {
-		if len(path) == n {
-			copyPath := make([]int, len(path))
-			copy(copyPath, path)
-			ans = append(ans, copyPath)
+		if startIdx == n {
+			ans = append(ans, append([]int{}, path...))
 			return
 		}
-		for i := startIdx; i < n; i++ {
-			path = append(path, nums[i])
-			backtrack(startIdx + 1)
-			path = path[:len(path)-1]
+		for idx, on := range used {
+			if !on {
+				path[startIdx] = nums[idx]
+				used[idx] = true
+				backtrack(startIdx + 1)
+				used[idx] = false
+			}
 		}
 	}
 	backtrack(0)
 	return ans
 }
 
+func permute1(nums []int) (ans [][]int) {
+	n := len(nums)
+	path := []int{}
+	used := make([]bool, n)
+	var dfs func(startIdx int)
+	dfs = func(startIdx int) {
+		if len(path) == n {
+			ans = append(ans, append([]int{}, path...))
+			return
+		}
+		for i := 0; i < n; i++ {
+			if !used[i] {
+				path = append(path, nums[i])
+				used[i] = true
+				dfs(i + 1)
+				used[i] = false
+				path = path[:len(path)-1]
+			}
+		}
+	}
+	dfs(0)
+	return
+}
+
 func main() {
 	nums := []int{1, 2, 3}
-	fmt.Println(permute(nums))
+	fmt.Println(permute1(nums))
 
 }
