681c45e6577a615fb0c26b3512fc414416cff897
Stuip <tangjmee@gmail.com>
2023-07-04 10:21:39 +0800

    Pass LRU

---
+++ LRU/main.go
@@ -0,0 +1,83 @@
+package main
+
+type LRUCache struct {
+	cache      map[int]*DLinkedNode
+	size       int
+	capacity   int
+	head, tail *DLinkedNode
+}
+
+type DLinkedNode struct {
+	key, value int
+	prev, next *DLinkedNode
+}
+
+func initDLinkedNode(key, value int) *DLinkedNode {
+	return &DLinkedNode{
+		key:   key,
+		value: value,
+	}
+}
+
+func Constructor(capacity int) LRUCache {
+	l := LRUCache{
+		cache:    map[int]*DLinkedNode{},
+		capacity: capacity,
+		size:     0,
+		head:     initDLinkedNode(0, 0),
+		tail:     initDLinkedNode(0, 0),
+	}
+	l.head.next = l.tail
+	l.tail.prev = l.head
+	return l
+}
+
+func (this *LRUCache) Get(key int) int {
+	if _, ok := this.cache[key]; !ok {
+		return -1
+	}
+	node := this.cache[key]
+	this.moveToHead(node)
+	return node.value
+}
+
+func (this *LRUCache) Put(key int, value int) {
+	if _, ok := this.cache[key]; !ok {
+		node := initDLinkedNode(key, value)
+		this.cache[key] = node
+		this.addToHead(node)
+		this.size += 1
+		if this.size > this.capacity {
+			removed := this.removeTail()
+			delete(this.cache, removed.key)
+			this.size -= 1
+		}
+	} else {
+		node := this.cache[key]
+		node.value = value
+		this.moveToHead(node)
+	}
+}
+
+func (this *LRUCache) removeTail() *DLinkedNode {
+	node := this.tail.prev
+	this.removeNode(node)
+	return node
+}
+
+func (this *LRUCache) moveToHead(node *DLinkedNode) {
+	this.removeNode(node)
+	this.addToHead(node)
+}
+
+func (this *LRUCache) removeNode(node *DLinkedNode) {
+	node.next.prev = node.prev
+	node.prev.next = node.next
+}
+
+func (this *LRUCache) addToHead(node *DLinkedNode) {
+	node.next = this.head.next
+	node.prev = this.head
+	this.head.next.prev = node
+	this.head.next = node
+}
