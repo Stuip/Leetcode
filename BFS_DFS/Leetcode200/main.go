b1a66bf5fee3162a4e90369fc0bcc6e9840dd653
Stuip <tangjmee@gmail.com>
2023-07-06 10:42:43 +0800

    Pass Leetcode200 BFS

--- BFS_DFS/Leetcode200/main.go
+++ BFS_DFS/Leetcode200/main.go
@@ -12,7 +12,7 @@ func numIslands(grid [][]byte) int {
 		for j := 0; j < cols; j++ {
 			if grid[i][j] == '1' {
 				ans += 1
-				dfs(grid, i, j, rows, cols)
+				bfs(grid, i, j, rows, cols)
 			}
 		}
 	}
@@ -33,12 +33,38 @@ func dfs(grid [][]byte, row, col int, rows, cols int) {
 	dfs(grid, row, col-1, rows, cols)
 }
 
+func bfs(grid [][]byte, row, col int, rows, cols int) {
+	queue := make([][2]int, 0)
+	queue = append(queue, [2]int{row, col})
+	for len(queue) != 0 {
+		coor := queue[0]
+		queue = queue[1:]
+		x, y := coor[0], coor[1]
+		if x+1 < rows && grid[x+1][y] == '1' {
+			grid[x+1][y] = '0'
+			queue = append(queue, [2]int{x + 1, y})
+		}
+		if x-1 >= 0 && grid[x-1][y] == '1' {
+			grid[x-1][y] = '0'
+			queue = append(queue, [2]int{x - 1, y})
+		}
+		if y+1 < cols && grid[x][y+1] == '1' {
+			grid[x][y+1] = '0'
+			queue = append(queue, [2]int{x, y + 1})
+		}
+		if y-1 >= 0 && grid[x][y-1] == '1' {
+			grid[x][y-1] = '0'
+			queue = append(queue, [2]int{x, y - 1})
+		}
+	}
+}
+
 func main() {
 	grid := [][]byte{
-		{'1', '1', '1', '1', '0'},
-		{'1', '1', '0', '1', '0'},
 		{'1', '1', '0', '0', '0'},
-		{'0', '0', '0', '0', '0'},
+		{'1', '1', '0', '0', '0'},
+		{'0', '0', '1', '0', '0'},
+		{'0', '0', '0', '1', '1'},
 	}
 	fmt.Println(numIslands(grid))
 }
