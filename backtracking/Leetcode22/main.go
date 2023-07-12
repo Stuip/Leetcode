2cb6fd4cc1766beb7bf1e319634a1568bfb8325c
Stuip <tangjmee@gmail.com>
2023-07-12 20:26:20 +0800

    Pass Leetcode22

--- backtracking/Leetcode22/main.go
+++ backtracking/Leetcode22/main.go
@@ -4,6 +4,23 @@ import "fmt"
 
 func generateParenthesis(n int) []string {
 	ans := []string{}
+	var backtrack func(left, right int, str string)
+	backtrack = func(left, right int, str string) {
+		if right > left {
+			return
+		}
+		if left == n && right == n {
+			ans = append(ans, str)
+			return
+		}
+		if left < n {
+			backtrack(left+1, right, str+"(")
+		}
+		if right < left {
+			backtrack(left, right+1, str+")")
+		}
+	}
+	backtrack(0, 0, "")
 	return ans
 }
 
