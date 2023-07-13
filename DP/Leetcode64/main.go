4b353a4ba3f6138a872b80ff0a58420b91c65617
Stuip <tangjmee@gmail.com>
2023-07-13 19:59:00 +0800

    Reproduce the code

--- DP/Leetcode64/main.go
+++ DP/Leetcode64/main.go
@@ -4,24 +4,20 @@ import "fmt"
 
 func minPathSum(grid [][]int) int {
 	m, n := len(grid), len(grid[0])
-	dp := make([][]int, m)
 	for i := 0; i < m; i++ {
-		dp[i] = make([]int, n)
-		if i == 0 {
-			dp[i][0] = grid[i][0]
-		} else {
-			dp[i][0] = dp[i-1][0] + grid[i][0]
+		for j := 0; j < n; j++ {
+			if i == 0 && j == 0 {
+				continue
+			} else if i == 0 {
+				grid[i][j] = grid[i][j-1] + grid[i][j]
+			} else if j == 0 {
+				grid[i][j] = grid[i-1][j] + grid[i][j]
+			} else {
+				grid[i][j] = grid[i][j] + min(grid[i-1][j], grid[i][j-1])
+			}
 		}
 	}
-	for j := 1; j < n; j++ {
-		dp[0][j] = dp[0][j-1] + grid[0][j]
-	}
-	for i := 1; i < m; i++ {
-		for j := 1; j < n; j++ {
-			dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1])
-		}
-	}
-	return dp[m-1][n-1]
+	return grid[m-1][n-1]
 }
 
 func min(a, b int) int {
