15347871e8271383e8fb1912d5b6681d0d767108
Stuip <tangjmee@gmail.com>
2023-07-17 16:07:47 +0800

    Simple the code

--- DP/Leetcode62/main.go
+++ DP/Leetcode62/main.go
@@ -20,6 +20,26 @@ func uniquePaths(m int, n int) int {
 	return dp[m-1][n-1]
 }
 
+func uniquePaths1(m int, n int) int {
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+	dp[0][0] = 1
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if i > 0 && j > 0 {
+				dp[i][j] = dp[i-1][j] + dp[i][j-1]
+			} else if i > 0 {
+				dp[i][j] = dp[i-1][j]
+			} else if j > 0 {
+				dp[i][j] = dp[i][j-1]
+			}
+		}
+	}
+	return dp[m-1][n-1]
+}
+
 func main() {
-	fmt.Println(uniquePaths(3, 7))
+	fmt.Println(uniquePaths1(3, 7))
 }
--- DP/Leetcode63/main.go
+++ DP/Leetcode63/main.go
@@ -32,6 +32,33 @@ func uniquePathsWithObstacles(obstacleGrid [][]int) int {
 	return memo[m-1][n-1]
 }
 
+func uniquePathsWithObstacles1(obstacleGrid [][]int) int {
+	m, n := len(obstacleGrid), len(obstacleGrid[0])
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+	if obstacleGrid[0][0] == 1 {
+		dp[0][0] = 0
+	} else {
+		dp[0][0] = 1
+	}
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if obstacleGrid[i][j] != 1 {
+				if i > 0 && j > 0 {
+					dp[i][j] = dp[i-1][j] + dp[i][j-1]
+				} else if i > 0 {
+					dp[i][j] = dp[i-1][j]
+				} else if j > 0 {
+					dp[i][j] = dp[i][j-1]
+				}
+			}
+		}
+	}
+	return dp[m-1][n-1]
+}
+
 func main() {
 	obstacleGrid := [][]int{
 		{0, 0},
