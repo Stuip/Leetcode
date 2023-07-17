fb757aea4a069fa56361c849a20a71da9d4c4df1
Stuip <tangjmee@gmail.com>
2023-07-17 16:08:11 +0800

    Simple the code

--- DP/Leetcode416/main.go
+++ DP/Leetcode416/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func canPartition(nums []int) bool {
 	sum := 0
@@ -20,6 +22,38 @@ func canPartition(nums []int) bool {
 	return dp[target] == target
 }
 
+func canPartition1(nums []int) bool {
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+	if sum%2 == 1 {
+		return false
+	}
+	target := sum / 2
+	n := len(nums)
+	dp := make([][]int, n)
+	for i := 0; i < n; i++ {
+		dp[i] = make([]int, target+1)
+		dp[i][0] = 0
+	}
+	for j := target; j >= 0; j-- {
+		if j >= nums[0] {
+			dp[0][j] = nums[0]
+		}
+	}
+	for i := 1; i < n; i++ {
+		for j := target; j >= 1; j-- {
+			if j >= nums[i] {
+				dp[i][j] = max(dp[i-1][j], dp[i-1][j-nums[i]]+nums[i])
+			} else {
+				dp[i][j] = dp[i-1][j]
+			}
+		}
+	}
+	return dp[n-1][target] == target
+}
+
 func max(a, b int) int {
 	if a > b {
 		return a
@@ -28,6 +62,6 @@ func max(a, b int) int {
 }
 
 func main() {
-	nums := []int{1, 5, 11, 5}
-	fmt.Println(canPartition(nums))
+	nums := []int{1, 5, 10, 6}
+	fmt.Println(canPartition1(nums))
 }
