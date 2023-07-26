daf8980173fd4a729bbb0ffe31df6a8c794bfdb0
Stuip <tangjmee@gmail.com>
2023-07-26 10:16:30 +0800

    Pass Leetcode1289

--- DP/Leetcode1289/main.go
+++ DP/Leetcode1289/main.go
@@ -8,20 +8,33 @@ import (
 func minFallingPathSum(grid [][]int) int {
 	n := len(grid)
 	dp := make([][]int, n)
+	l1, l2 := 0, 0 // 最小值的索引，次小值的索引
 	for i := 0; i < n; i++ {
 		dp[i] = make([]int, n)
 		dp[0][i] = grid[0][i]
+		if grid[0][i] < grid[0][l1] {
+			l1 = i
+			l2 = i
+		} else if grid[0][i] < grid[0][l2] {
+			l2 = i
+		}
 	}
 	for i := 1; i < n; i++ {
+		newL1, newL2 := 0, 0
 		for j := 0; j < n; j++ {
-			dp[i][j] = math.MaxInt
-
-			for k := 0; k < n; k++ {
-				if k != j {
-					dp[i][j] = min(dp[i][j], dp[i-1][k]+grid[i][j])
-				}
+			if j == l1 {
+				dp[i][j] = grid[i][j] + dp[i][l2]
+			} else {
+				dp[i][j] = grid[i][j] + dp[i][l1]
+			}
+			if dp[i][j] < dp[i][newL1] {
+				newL1 = j
+				newL2 = j
+			} else if dp[i][j] < dp[i][newL2] {
+				newL2 = j
 			}
 		}
+		l1, l2 = newL1, newL2
 	}
 	ans := math.MaxInt
 	for i := 0; i < n; i++ {
@@ -39,11 +52,9 @@ func min(a, b int) int {
 
 func main() {
 	matrix := [][]int{
-		{-73, 61, 43, -48, -36},
-		{3, 30, 27, 57, 10},
-		{96, -76, 84, 59, -15},
-		{5, -49, 76, 31, -7},
-		{97, 91, 61, -46, 67},
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
 	}
 	fmt.Println(minFallingPathSum(matrix))
 }
