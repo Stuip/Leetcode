3d97b3df5f21a3f2e3a9ad107c6d1e3173c25c7d
Stuip <tangjmee@gmail.com>
2023-09-07 10:22:22 +0800

    Pass P2858 区间DP

---
+++ DP/区间DP/P2858-Treats for the Cows/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scanln(&n)
+	prices := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scanln(&prices[i])
+	}
+	fmt.Println(treats(prices))
+}
+
+func treats(prices []int) int {
+	n := len(prices)
+	dp := make([][]int, n)
+	for i := 0; i < n; i++ {
+		dp[i] = make([]int, n)
+		dp[i][i] = prices[i] * n
+	}
+	for length := 2; length <= n; length++ { // 天数为2
+		for left := 0; left+length <= n; left++ {
+			right := left + length - 1
+			age := n - length + 1
+			a := prices[left]*age + dp[left+1][right]
+			b := prices[right]*age + dp[left][right-1]
+			dp[left][right] = max(a, b)
+		}
+	}
+	return dp[0][n-1]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
