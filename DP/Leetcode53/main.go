3b8152557a29d2650d617fd1a99463adf8e04fb3
Stuip <tangjmee@gmail.com>
2023-10-30 11:18:12 +0800

    Add Divide and conquer method.

--- DP/Leetcode53/main.go
+++ DP/Leetcode53/main.go
@@ -19,6 +19,38 @@ func maxSubArray1(nums []int) int {
 	return ans
 }
 
+func maxSubArray2(nums []int) int {
+	return getMax(nums, 0, len(nums)-1)
+}
+
+func getMax(nums []int, left, right int) int {
+	if left == right {
+		return nums[left]
+	}
+	mid := left + (right-left)/2
+	leftMax := getMax(nums, left, mid)
+	rightMax := getMax(nums, mid+1, right)
+	crossMax := getCrossMax(nums, left, right)
+	return max(max(leftMax, rightMax), crossMax)
+}
+
+func getCrossMax(nums []int, left, right int) int {
+	mid := left + (right-left)/2
+	leftSum := nums[mid]
+	leftMax := leftSum
+	for i := mid - 1; i >= left; i-- {
+		leftSum += nums[i]
+		leftMax = max(leftMax, leftSum)
+	}
+	rightSum := nums[mid+1]
+	rightMax := rightSum
+	for i := mid + 2; i <= right; i++ {
+		rightSum += nums[i]
+		rightMax = max(rightMax, rightSum)
+	}
+	return leftMax + rightMax
+}
+
 func maxSubArray(nums []int) int {
 	// dp[i]数组保存着， 以索引i为结尾的最大子序列和
 	n := len(nums)
@@ -40,5 +72,8 @@ func max(a, b int) int {
 }
 
 func main() {
-	fmt.Println(maxSubArray([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4}))
+	nums := []int{5, 4, -1, 7, 8}
+	fmt.Println(maxSubArray(nums))
+	fmt.Println(maxSubArray1(nums))
+	fmt.Println(maxSubArray2(nums))
 }
